bd3a3c06a30a3c3f9de8a2c35535f41381aea612
Tavitu97 <43606487+Tavitu97@users.noreply.github.com>
2022-03-29 19:42:23 +0300

    Enemy load animations

--- Gameplay/Enemy.cs
+++ Gameplay/Enemy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lonernot.Engine;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Lonernot
 {
-    class Enemy
+    public class Enemy: AnimationManager 
     {
+
+        public float mSpeed = 1.2f;
+        
+        public Enemy(Dictionary<string, Animation> animations,Vector2 position) : base(animations)
+        {
+            this._animations = animations;
+            this._animationManager = new AnimationManager(animations);
+            this.Position = position;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- States/GameState.cs
+++ States/GameState.cs
@@ -14,10 +14,13 @@ namespace Lonernot.States
     {
         public Map map;
         public Player player;
+        public Enemy enemy;
 
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
             map = new Map(content, "Content/Lonernot.tmx");
+
+            // loading animations for player
             var playerAnimations = new Dictionary<string, Animation>()
             {
                 {"Walking_up", new Animation(content.Load<Texture2D>("Spritesheets/Player/Walking_up"),4,1) },
@@ -25,7 +28,26 @@ namespace Lonernot.States
                 {"Walking_right", new Animation(content.Load<Texture2D>("Spritesheets/Player/Walking_right"),4,1) },
                 {"Walking_left", new Animation(content.Load<Texture2D>("Spritesheets/Player/Walking_left"),4,1) }
             };
+
+            //loading animations for enemy
+            var enemyAnimations = new Dictionary<string, Animation>()
+            {
+                {"Walking_up", new Animation(content.Load<Texture2D>("Spritesheets/Enemy/Walking_up"),4,1) },
+                {"Walking_down", new Animation(content.Load<Texture2D>("Spritesheets/Enemy/Walking_down"),4,1) },
+                {"Walking_right", new Animation(content.Load<Texture2D>("Spritesheets/Enemy/Walking_right"),4,1) },
+                {"Walking_left", new Animation(content.Load<Texture2D>("Spritesheets/Enemy/Walking_left"),4,1) }
+
+            };
+
+
+
+
+            // create player
             player = new Player(playerAnimations);
+
+            //create enemy
+            enemy = new Enemy(enemyAnimations);
+
             // Buttons Creation
             var buttonTexture = _content.Load<Texture2D>("Controls/button3");
             var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
